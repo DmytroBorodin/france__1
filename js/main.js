5c0e530c376909f8cfd244df1206aba57c56316a
Dmytro Borodin <48290702+DmytroBorodin@users.noreply.github.com>
2020-05-24 11:49:12 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
+'use strict'
 
+let menuBlock = document.querySelector('.menu__block');
+let burger = document.querySelector('.burger');
 
-
+burger.addEventListener('click', () => {
+	menuBlock.classList.toggle('menu__block__active');
+})
 
